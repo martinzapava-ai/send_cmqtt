e277f1f0208ed91a9df15e71262e833c1528569a
martinzapava-ai <martinzapava@gmail.com>
2026-04-23 14:25:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.title("MQTT Control")
 
 if st.button('ON'):
     act1="ON"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("Martin")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
